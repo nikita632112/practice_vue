8b7d3437b46fc39eb55ae17481be2de74492ec23
Nikita Strekalov <nikita.strekalov123@gmail.com>
2025-06-23 18:06:22 +0400

    feat: add task description

--- src/views/product-catalog.vue
+++ src/views/product-catalog.vue
@@ -104,17 +104,7 @@
               <p><button type="button" class="btn btn-primary">Заказать</button></p>
             </div>
           </div>
-          <div class="card text-center card-product">
-            <div class="card-product__img">
-              <img class="card-img" src="https://placeimg.dev/255/255/nature" alt="" />
-            </div>
-            <div class="card-body">
-              <p>Россия</p>
-              <h4 class="card-product__title">Сочи</h4>
-              <p class="card-product__price">200 000 руб</p>
-              <p><button type="button" class="btn btn-primary">Заказать</button></p>
-            </div>
-          </div>
+
           <div class="card text-center card-product">
             <div class="card-product__img">
               <img class="card-img" src="https://placeimg.dev/255/255/nature" alt="" />
